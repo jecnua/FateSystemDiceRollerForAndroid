7a01adf2d613b97c1fdefca67cd278da642e16f8
jecnua <jecnua@gmail.com>
2012-09-12 11:47:48 +0100

    Minor changes.

--- src/com/jecnua/fatesystemdiceroller/DiceRoller.java
+++ src/com/jecnua/fatesystemdiceroller/DiceRoller.java
@@ -1,7 +1,5 @@
 package com.jecnua.fatesystemdiceroller;
 
-import java.util.ArrayList;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -12,13 +10,9 @@ import android.widget.TextView;
 
 public class DiceRoller extends Activity {
 
+	
 	TextView resultTextView;
-	ArrayList<Button> allButtons = new ArrayList<Button>();
-	TextView firstDice;
-	TextView secondDice;
-	TextView thirdDice;
-	TextView fourthDice;
-	TextView totalOfTheDice;
+	
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,128 +21,7 @@ public class DiceRoller extends Activity {
         setContentView(R.layout.activity_dice_roller);
         
         resultTextView = (TextView)findViewById(R.id.displayResult);
-//        resultTextView.setText((CharSequence)"Result");
         
-        /* Find Tablelayout defined in main.xml */
-//        TableLayout tl = (TableLayout)findViewById(R.id.skillMatrix);
-        
-        
-        
-        /*
-        Integer maxSkillAvaible = 9;
-        TableRow temporaryTableRow = null;
-        
-        
-        //Text Area for each dice in a single row
-        
-		temporaryTableRow = new TableRow(this);
-        temporaryTableRow.setLayoutParams(new LayoutParams(
-        		LayoutParams.FILL_PARENT,
-        		LayoutParams.WRAP_CONTENT));
-        
-        tl.addView(temporaryTableRow,new TableLayout.LayoutParams(
-                  LayoutParams.FILL_PARENT,
-                  LayoutParams.WRAP_CONTENT));
-        
-    	 firstDice = new TextView(this);
-    	 secondDice = new TextView(this);
-    	 thirdDice = new TextView(this);
-    	 fourthDice = new TextView(this);
-        
-        temporaryTableRow.addView(firstDice);
-        temporaryTableRow.addView(secondDice);
-        temporaryTableRow.addView(thirdDice);
-        temporaryTableRow.addView(fourthDice);
-        
-        
-        //Text area for the result of the dice on a row
-        
-		temporaryTableRow = new TableRow(this);
-        temporaryTableRow.setLayoutParams(new LayoutParams(
-        		LayoutParams.FILL_PARENT,
-        		LayoutParams.WRAP_CONTENT));
-        
-        tl.addView(temporaryTableRow,new TableLayout.LayoutParams(
-                  LayoutParams.FILL_PARENT,
-                  LayoutParams.WRAP_CONTENT));
-        
-        totalOfTheDice = new TextView(this);
-        temporaryTableRow.addView(totalOfTheDice);
-        
-        //Creating rows and buttons for the skills
-        
-        for (Integer counter=0; counter < maxSkillAvaible; counter++){
-        	
-        	Integer value = counter+1;
-        	
-        	if ((counter%3) == 0){
-        		// Create a new row to be added.
-		        temporaryTableRow = new TableRow(this);
-		        temporaryTableRow.setLayoutParams(new LayoutParams(
-		        		LayoutParams.FILL_PARENT,
-		        		LayoutParams.WRAP_CONTENT));
-		        
-		        // Add row to TableLayout.
-		        tl.addView(temporaryTableRow,new TableLayout.LayoutParams(
-		                  LayoutParams.FILL_PARENT,
-		                  LayoutParams.WRAP_CONTENT));
-        	}
-	        
-        	// Create a Button to be the row-content.
-	        Button temporaryButton = new Button(this);
-	        temporaryButton.setText(value.toString());
-	        temporaryButton.setTag(Integer.valueOf(value));
-	        temporaryButton.setWidth(80);
-	        temporaryButton.setHeight(80);
-	        
-	        //OnClick event manager
-	        temporaryButton.setOnClickListener(new View.OnClickListener(){
-	            public void onClick(View v) {
-	            	Button n = (Button)v;
-	            	Integer g = (Integer)n.getTag();
-	            	
-	            	FateSystemUtilities FSU = new FateSystemUtilities();
-	            	FSU.throwDices();
-	            	int value = g+FSU.resultTotalDices;
-	            	
-	            	resultTextView.setText((CharSequence)"");
-	                resultTextView.setText(""+value);
-	                
-	                for(Button aButton: allButtons){
-	                	aButton.setText(""+aButton.getTag());
-	                }
-	                
-	                //Set new background color
-	                //n.setBackgroundColor(Color.BLUE);
-	                n.setText("["+n.getText()+"]");
-	                
-	                firstDice.setText(""+FSU.resultForSingleDice[0]);
-	                secondDice.setText(""+FSU.resultForSingleDice[1]);
-	                thirdDice.setText(""+FSU.resultForSingleDice[2]);
-	                fourthDice.setText(""+FSU.resultForSingleDice[3]);
-	                
-	                
-	                totalOfTheDice.setText(""+FSU.resultTotalDices);
-	                
-	            }
-	        });
-	        */
-	        
-	        
-	        /*
-	        temporaryButton.setLayoutParams(new LayoutParams(
-	        		LayoutParams.FILL_PARENT,
-	        		LayoutParams.WRAP_CONTENT)
-	        );
-	        */
-	        
-        /*
-	        // Add Button to row.
-	        temporaryTableRow.addView(temporaryButton);
-	        
-	        allButtons.add(temporaryButton);
-        }
-        */
     }
     
     
@@ -164,26 +37,11 @@ public class DiceRoller extends Activity {
     	resultTextView.setText((CharSequence)"");
         resultTextView.setText(""+value);
         
-//        for(Button aButton: allButtons){
-//        	aButton.setText(""+aButton.getTag());
-//        }
-        
-        //Set new background color
+        //Show witch button has been pressed last
         //n.setBackgroundColor(Color.BLUE);
 //        n.setText("["+n.getText()+"]");
         
-        /*
-        Button tempB = (Button)findViewById(R.id.firstDice);
-        
-        tempB.setText(""+FSU.resultForSingleDice[0]);
-        tempB = (Button)findViewById(R.id.secondDice);
-        tempB.setText(""+FSU.resultForSingleDice[1]);
-        tempB = (Button)findViewById(R.id.thirdDice);
-        tempB.setText(""+FSU.resultForSingleDice[2]);
-        tempB = (Button)findViewById(R.id.fourthDice);
-        tempB.setText(""+FSU.resultForSingleDice[3]);
-        */
-        
+        //Get the result of each dice and update the corresponding image
         ImageView i = (ImageView)findViewById(R.id.firstDice);
         changeImage(i, FSU.resultForSingleDice[0]);
         i = (ImageView)findViewById(R.id.secondDice);
@@ -193,7 +51,9 @@ public class DiceRoller extends Activity {
         i = (ImageView)findViewById(R.id.fourthDice);
         changeImage(i, FSU.resultForSingleDice[3]);
         
+        //Show the total result of the dice
 //        totalOfTheDice.setText(""+FSU.resultTotalDices);
+        
     }
 
     
@@ -207,11 +67,11 @@ public class DiceRoller extends Activity {
     }
     
     
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_dice_roller, menu);
         return true;
     }
 
+    
 }
